83e89dc7c17af20e6c766f6687d397cee0b248b6
James Chen <james.chen208@gmail.com>
2016-07-09 16:38:56 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.modal-trigger').leanModal();
+    $('.parallax').parallax();
+    $('.slider').slider({height: 600});
+    $('.carousel').carousel();
+    $('.materialboxed').materialbox();
+    $(".button-collapse").sideNav();
+});
